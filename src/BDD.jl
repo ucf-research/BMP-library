84db69f94161306b9375994759402e7790bfa342
erenust <erenust@gmail.com>
2025-01-27 14:37:09 -0500

    Multiple BDD outputs and evaluation

--- src/BDD.jl
+++ src/BDD.jl
@@ -1,12 +1,13 @@
 const BDDNode = Tuple{UInt32, UInt32, UInt32}
 struct BDD
     nodes::Vector{BDDNode}
+    outputs::Vector{UInt32}
     order::Vector{UInt32}
     position::Vector{UInt32}
-    function BDD(nodes, order)
+    function BDD(nodes, outputs, order)
         position = fill(UInt32(0), length(order))
         position[order] .= order
-        return new(nodes, order, position)
+        return new(nodes, copy(outputs), copy(order), position)
     end
 end
 
@@ -14,7 +15,7 @@ function Base.length(bdd::BDD)
     return length(bdd.order)
 end
 
-function BDD_reduce!(bdd::BDD)
+function BDD_reduce(bdd::BDD)
     n = length(bdd)
     V = length(bdd.nodes)
     sorted = sortperm(bdd.nodes, by=x->x[1], rev=true)
@@ -33,8 +34,31 @@ function BDD_reduce!(bdd::BDD)
             ids[node_ind] = get!(id_map, (var_idx, lid, rid), length(id_map)+1)
         end
     end
-    resize!(bdd.nodes, length(id_map))
+    newnodes = Vector{BDDNode}(undef, length(id_map))
     for (k,v) in pairs(id_map)
-        bdd.nodes[v] = k
+        newnodes[v] = k
     end
+    newouts = [ids[i] for i in bdd.outputs]
+    return BDD(newnodes, newouts, bdd.order)
+end
+
+function eval(bdd::BDD, x::BitArray)
+    n = length(bdd)
+    m = length(bdd.outputs)
+    n_samps = div(length(x), size(x, 1))
+    x_ = reshape(x, (n, n_samps))
+    result = BitArray(undef, (m, n_samps))
+    for j=1:n_samps
+        for k=1:m
+            node = bdd.nodes[bdd.outputs[k]]
+            while node[1] != n+1
+                next_ref = x_[node[1], j] == 0 ? node[2] : node[3]
+                node = bdd.nodes[next_ref]
+            end
+            result[k, j] = node[3]
+        end
+    end
+    shape = [i for i in size(x)]
+    shape[1] = m
+    return reshape(result, Tuple(shape))
 end
--- test/test_bdd.jl
+++ test/test_bdd.jl
@@ -11,15 +11,22 @@ let
         (4, 0, 1),
         (4, 0, 0),
         (4, 0, 1)
-    ], [1,2,3])
-    for (i, n) in enumerate(bdd.nodes)
-        nn = Tuple{Int64, Int64, Int64}(n)
+    ], [1], [1,2,3])
+    for (i, nd) in enumerate(bdd.nodes)
+        nn = Tuple{Int64, Int64, Int64}(nd)
         println("\t$i\t$nn")
     end
     println()
-    BDD_reduce!(bdd)
-    for (i, n) in enumerate(bdd.nodes)
-        nn = Tuple{Int64, Int64, Int64}(n)
+    newbdd = BDD_reduce(bdd)
+    for (i, nd) in enumerate(newbdd.nodes)
+        nn = Tuple{Int64, Int64, Int64}(nd)
         println("\t$i\t$nn")
     end
+    println()
+    n = 3
+    test_input = BitArray(undef, (n, 2^n))
+    for j=1:n, i=0:2^n-1
+        test_input[j, i+1] = i >> (n-j) & 1
+    end
+    @show all(eval(bdd, test_input) .== eval(newbdd, test_input))
 end
