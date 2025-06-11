645ca6c6491fead3187fd34e148ca6fbf6864fcc
erenust <erenust@gmail.com>
2025-06-11 17:16:10 -0400

    Load the module locally for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using Documenter
-push!(LOAD_PATH, "../src/")
-using BinaryMatrixProducts
+# push!(LOAD_PATH, "../src/")
+include("../src/BinaryMatrixProducts.jl")
+using .BinaryMatrixProducts
 
 makedocs(
     sitename="BMP Library Documentation",
